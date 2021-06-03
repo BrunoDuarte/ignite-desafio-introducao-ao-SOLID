81b8cee99137eae120150801a6d5ece306c68a0f
Bruno Duarte <francisco.duarte@bairesdev.com>
2021-06-02 23:43:30 -0300

    challenge accepted

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,7 +10,13 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const emailExists = this.usersRepository.findByEmail(email)
+
+    if (emailExists) throw new Error("Email already in use!")
+
+    const newUser = this.usersRepository.create({name, email})
+
+    return newUser
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -1,3 +1,4 @@
+import { response } from "express";
 import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
@@ -9,7 +10,13 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const userbyId = this.usersRepository.findById(user_id)
+
+    if (!userbyId.admin) throw new Error("Action not allowed for your user!")
+
+    const allUsers = this.usersRepository.list()
+
+    return allUsers
   }
 }
 
